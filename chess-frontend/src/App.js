5fd161288e84b76709ab002a440686eb70015184
mikejaklitsch <mjaklitsch123@gmail.com>
2024-10-23 16:54:33 -0700

    Simply Stylized Board
    Added shadowing and gray background to board. Centered board on resizable display with standardized margins.

--- chess-frontend/src/App.js
+++ chess-frontend/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { Chessboard } from "react-chessboard";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      width: "calc(100vw - 120px)",        // Full viewport width minus the margin
+      height: "calc(100vw - 120px)",       // Set height equal to width for square aspect minus the margin
+      maxHeight: "calc(100vh - 120px)",    // Limit height to viewport height minus the margin
+      maxWidth: "calc(100vh - 120px)",     // Limit width to viewport width minus the margin
+      margin: "60px auto",                 // Center with equal margins on top/bottom and left/right
+    }}>
+        <Chessboard id="BasicBoard" style={{
+            width: "100%",  // Fill the container width
+            height: "100%", // Fill the container height
+        }} />
     </div>
   );
 }
